c8fc3904cfe28d058af4e5bbd45972d408c1af1c
Yoshi nossro <107130115+Kurdt-Komeng@users.noreply.github.com>
2022-06-16 23:50:28 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 echo -e "\e[1;34m[❌] \e[32mUpdate dan upgrading apt dan pkg sila tunggu.. \e[0m"
 sleep 2
  pkg update -y && pkg upgrade -y && apt update -y && apt upgrade -y
-sleep 1
- echo -e "\e[1;34m[✅]\e[32mapt dan pkg berjaya update dan upgrade."
+sleep 2
+clear
+echo -e "\e[1;34m[✅]\e[32mapt dan pkg berjaya update dan upgrade."
+
+
